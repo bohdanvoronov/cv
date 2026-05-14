715d6683f4ee78d4dfa0be3fe91c2a2638c331d5
bohdanvoronov <bohdanvvoronov@gmail.com>
2026-05-14 22:34:53 +0200

    middleware added

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import sqlite3
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 conn = sqlite3.connect('portfolio.db', check_same_thread=False)
 cursor = conn.cursor()
 
